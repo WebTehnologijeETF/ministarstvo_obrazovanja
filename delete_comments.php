6edacf32f9601719436876d4819b28d2ae0b4be0
denisdzafo <denisdzafo@gmail.com>
2015-05-29 00:08:16 +0200

    nesto

--- delete_comments.php
+++ delete_comments.php
@@ -1,47 +1,27 @@
+
 <?php
 
 
-
-include('mysql_connect2.php');
-
-$query = "SELECT id, naslov FROM news_posts";
-
-$result = @mysql_query($query);
-
-
+    include ('mysql_connect2.php');
 if (isset($_GET['id'])) {
-
-
-if (is_numeric($_GET['id'])) {
 
 $id = $_GET['id'];
 
-$query = "DELETE FROM comments WHERE id = $id";
-
-$result = mysql_query($query);
-
-if ($result) {
-
-echo '<h3>Uspješno!</h3><br />
-
-Vijesti su uspješno obrisane.<br /><br />';
-
-}
-}
 }
 
-<form action="<?php $_SERVER['PHP_SELF']; ?>" method="post" />
+$query = "SELECT * FROM comments WHERE nid = $id";
 
-<p>Naslov komentara : <input type="text" name="naslov" maxlength="70" value="<?php if(isset($_POST['naslov'])) echo $_POST['autor'];?>" /></p>
+$result = @mysql_query($query);
 
-<p>Vaše ime : <input type="text" name="autor" length="25" maxlength="50" value="<?php if(isset($_POST['autor'])) echo $_POST['autor'];?>" /></p>
-
-<p>Komentar : <textarea columns="6" rows="6" name="komentar"><?php if(isset($_POST['komentar'])) echo $_POST['komentar'];?></textarea></p>
-
-<p><div align="center"><input type="submit" name="submit" value="Ostavite komentar" /></div></p>
-
-<input type="hidden" name="submitted" value="TRUE" />
-
-</form>
-
+    
+     $result = $query->prepare("delete from comments where id =:id;");
+	 $result->bindValue(":id", $comments, PDO::PARAM_INT);
+     $result->execute();    
+	if (!$result) {
+          $greska = $veza->errorInfo();
+          print "SQL greška: " . $greska[2];
+          exit();
+     }
+	 
+	 
 ?>
